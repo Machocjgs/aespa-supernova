182c7b8d43377bacf020dd1b3d2ed4cfb4dd307f
mattcjgs <matthew@freeborncapital.com>
2024-06-05 16:53:02 +0800

    Update category and add size

---
+++ client/src/services/sizes.service.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/api/size/';
+
+class SizeService {
+  findMany(params) {
+
+    console.log(params)
+    const payload = {
+      method: 'get',
+      url: API_URL,
+      headers: {'token': localStorage.getItem('token')},
+      params
+    }
+
+    return axios(payload)
+      .then(response => {
+        return response;
+      });
+  }
+
+  createSize(body){
+    return axios.post(`${API_URL}`, body, {headers: {'token': localStorage.getItem('token')}})
+  }
+
+  deleteSize(size_id) {
+    console.log(size_id);
+    return axios.delete(`${API_URL}/${size_id}`, {headers:{'token': localStorage.getItem('token')}});
+  }
+}
+
+export default new SizeService();
